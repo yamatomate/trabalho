388b2a0103bd5105d689689fb8f1dd17029f8a58
Gustavo Lisboa <130182272+yamatomate@users.noreply.github.com>
2024-05-29 03:07:58 -0300

    barra de pesquisa

---
+++ padrao.js
@@ -0,0 +1,32 @@
+const rotas = [
+    ["classico","benClassico.html"],
+    ["12",NaN],
+    ["22",NaN],
+    ["23",NaN]
+]
+const sites = new Map(rotas);
+console.log(sites.size);
+console.log(sites);
+function pesquisar1(){
+    var ipPes = document.getElementById('inputBP');
+    ipPes.style.borderRadius = "25px 0px 0px 0px";
+    var iptPesquisa = ipPes.value;
+    var pesqui = document.getElementById('pes');
+    pesqui.style.display = "flex";
+    pesqui.innerHTML = "<p></p>"
+    for (const [chave, caminho] of sites){
+        console.log(iptPesquisa + " <=> " + chave +"||"+caminho+ " => " + chave.localeCompare(iptPesquisa));
+        if ( chave.localeCompare(iptPesquisa) == 0) {
+            pesqui.innerHTML = `<a href=${caminho}>${chave}</a>`;
+        } else  {
+            pesqui.innerHTML += `<p>${chave}</p>`;
+        }
+    }
+
+}
+function restarBP(){
+    const inputBP = document.getElementById('inputBP');
+    const pesqui = document.getElementById('pes');
+    inputBP.style = "";
+    pesqui.style = "";
+}

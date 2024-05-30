2b816eba8bc8ea7f19ea136a4a701e7720da4f8a
Gustavo Lisboa <130182272+yamatomate@users.noreply.github.com>
2024-05-29 21:44:39 -0300

    butao de mdo escuro

--- padrao.js
+++ padrao.js
@@ -1,22 +1,30 @@
 const rotas = [
     ["classico","benClassico.html"],
     ["força alienigena","benForca.html"],
-    ["supremacia alienigena","benSupre.html"],
-    ["omniverse","benOmniverse.html"],
-    ["aliens","aliens/todosAliens.html"],
-    ["objetos", "objetos/todosObjetos.html"]
+    ["supremacia alienigena","benSupre.html"],     
+    ["omniverse","benOmniverse.html"],     
+    ["aliens","aliens/todosAliens.html"],    
+    ["objetos", "objetos/todosObjetos.html"],     
+    ["ben", "personagens/ben.html"],     
+    ["Azmuth", "personagens/Azmuth.html"],     
+    ["gwen", "personagens/gwen.html"],     
+    ["kevin", "personagens/kevin.html"],     
+    ["rook", "personagens/Rook.html"],     
+    ["tetrax", "personagens/tetrax.html"],     
+    ["vo max", "personagens/Vo_max.html"],     
+    ["formulario", "formulario.html"]
 ]
 const sites = new Map(rotas);
 console.log(sites.size);
 console.log(sites);
 
 function pesquisar1(){
-    var ipPes = document.getElementById('inputBP');
-    var btnPes = document.getElementById('btnBp');
+    const ipPes = document.getElementById('inputBP');
+    const btnPes = document.getElementById('btnBp');
     btnPes.style.borderRadius = "0px 25px 0px 0px";
     ipPes.style.borderRadius = "25px 0px 0px 0px";
-    var iptPesquisa = ipPes.value;
-    var pesqui = document.getElementById('pes');
+    const iptPesquisa = ipPes.value;
+    const pesqui = document.getElementById('pes');
     pesqui.style.display = "flex";
     pesqui.innerHTML = ""
     var opa = 0;
@@ -34,8 +42,22 @@ function pesquisar1(){
 function restarBP(){
     const inputBP = document.getElementById('inputBP');
     const pesqui = document.getElementById('pes');
-    var btnPes = document.getElementById('btnBp');
+    const btnPes = document.getElementById('btnBp');
     inputBP.style = "";
     pesqui.style = "";
     btnPes.style = "";
+}
+
+function mudarTema() {
+    console.log("teste")
+    const css = document.getElementById("idCss")
+    const iconCE = document.getElementById("bolsonaro_de_calcinha")
+    console.log(`${css}\n${iconCE}`)
+    if (css.getAttribute("href") == "nvPadrao.css"){
+        css.setAttribute("href","nvPadraoEscuro.css")
+        iconCE.setAttribute("src","imagens/moon-svgrepo-com.svg")
+    } else {
+        css.setAttribute("href","nvPadrao.css")
+        iconCE.setAttribute("src","imagens/sun-svgrepo-com.svg")
+    }
 }

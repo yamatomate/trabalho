49458b99e2641af07de83929765cfd60ce18735f
Gustavo Lisboa <130182272+yamatomate@users.noreply.github.com>
2024-06-02 16:47:16 -0300

    olha ai

--- iniciar.js
+++ iniciar.js
@@ -1,8 +1,2 @@
 var tema = sessionStorage.getItem("tema");
-let css = document.getElementById("idCss")
-console.log("gozei = ", tema)
-if (tema){
-    css.setAttribute("href", "temaClaro.css");
-} else{
-    css.setAttribute("href", "temaEscuro.css");
-}
+console.log("tema regastado: "+ tema)
--- principal.js
+++ principal.js
@@ -1,125 +1,131 @@
 const sites = [
-    ["classico","benClassico.html"],
-    ["força alienigena","benForca.html"],
-    ["supremacia alienigena","benSupre.html"],     
-    ["omniverse","benOmniverse.html"],     
-    ["aliens","aliens/todosAliens.html"],    
-    ["objetos", "objetos/todosObjetos.html"],     
-    ["ben", "personagens/ben.html"],     
-    ["azmuth", "personagens/Azmuth.html"],     
-    ["gwen", "personagens/gwen.html"],     
-    ["kevin", "personagens/kevin.html"],     
-    ["rook", "personagens/Rook.html"],     
-    ["tetrax", "personagens/tetrax.html"],     
-    ["vo max", "personagens/Vo_max.html"],     
-    ["formulario", "formulario.html"]
+  ["classico", "benClassico.html"],
+  ["força alienigena", "benForca.html"],
+  ["supremacia alienigena", "benSupre.html"],
+  ["omniverse", "benOmniverse.html"],
+  ["aliens", "aliens/todosAliens.html"],
+  ["objetos", "objetos/todosObjetos.html"],
+  ["ben", "personagens/ben.html"],
+  ["azmuth", "personagens/Azmuth.html"],
+  ["gwen", "personagens/gwen.html"],
+  ["kevin", "personagens/kevin.html"],
+  ["rook", "personagens/Rook.html"],
+  ["tetrax", "personagens/tetrax.html"],
+  ["vo max", "personagens/Vo_max.html"],
+  ["formulario", "formulario.html"],
 ];
 
-console.log(sites);
-console.log(sites.length);
 var escolhido = null;
-console.log(escolhido)
-function pesquisar1(){
-    const ipPes = document.getElementById('inputBP');
-    const btnPes = document.getElementById('btnBp');
-    btnPes.style.borderRadius = "0px 25px 0px 0px";
-    ipPes.style.borderRadius = "25px 0px 0px 0px";
-    const iptPesquisa = ipPes.value.toLowerCase();
-    const pesqui = document.getElementById('pes');
-    pesqui.style.display = "flex";
-    pesqui.innerHTML = ""
-    var opa = 0;
-    var i = 0;
-    escolhido = null;
-        console.log("começando varredura")
-        for (i = 0; i < sites.length; i++){
-            console.log(`${i} (${iptPesquisa} == ${sites[i][0]})=${sites[i][0].includes(iptPesquisa)}`);
-            if (!sites[i][0].includes(iptPesquisa)){
-                console.log("tipo1")
-            } else {
-                console.log("tipo2")
-                escolhido = sites[i];
-                if (opa < 5){
-                    pesqui.innerHTML += `<a href="${sites[i][1]}"> ${sites[i][0]} </a>`
-                    opa += 1;
-                }
-            }
-            console.log(escolhido)
-        }
-        console.log("varredura completa " + opa)
-    }
-function confirmar(){
-    var tavazio = escolhido != null
-    console.log(tavazio)
-    if (tavazio){
-        console.log("tem algo:" + escolhido);
-        window.open(escolhido[1], "_self");
+function pesquisar1() {
+  const ipPes = document.getElementById("inputBP");
+  const btnPes = document.getElementById("btnBp");
+  btnPes.style.borderRadius = "0px 25px 0px 0px";
+  ipPes.style.borderRadius = "25px 0px 0px 0px";
+  const iptPesquisa = ipPes.value.toLowerCase();
+  const pesqui = document.getElementById("pes");
+  pesqui.style.display = "flex";
+  pesqui.innerHTML = "";
+  let opa = 0;
+  let i = 0;
+  escolhido = null;
+  console.log("começando varredura");
+  for (i = 0; i < sites.length; i++) {
+    console.log(
+      `${i} (${iptPesquisa} == ${sites[i][0]})=${sites[i][0].includes(
+        iptPesquisa
+      )}`
+    );
+    if (!sites[i][0].includes(iptPesquisa)) {
+      console.log("tipo1");
     } else {
-        console.log("tem nada:" + escolhido);
+      console.log("tipo2");
+      escolhido = sites[i];
+      if (opa < 5) {
+        pesqui.innerHTML += `<a href="${sites[i][1]}"> ${sites[i][0]} </a>`;
+        opa += 1;
+      }
     }
+    console.log(escolhido);
+  }
+  console.log("varredura completa " + opa);
 }
-function restarBP(){
-    const inputBP = document.getElementById('inputBP');
-    const pesqui = document.getElementById('pes');
-    const btnPes = document.getElementById('btnBp');
-    inputBP.style = "";
-    pesqui.style = "";
-    btnPes.style = "";
+function confirmar() {
+  var tavazio = escolhido != null;
+  console.log(tavazio);
+  if (tavazio) {
+    console.log("tem algo:" + escolhido);
+    window.open(escolhido[1], "_self");
+  } else {
+    console.log("tem nada:" + escolhido);
+  }
+}
+function restarBP() {
+  const inputBP = document.getElementById("inputBP");
+  const pesqui = document.getElementById("pes");
+  const btnPes = document.getElementById("btnBp");
+  inputBP.style = "";
+  pesqui.style = "";
+  btnPes.style = "";
 }
 
-// mudar tema //
+// tema //
 
 const corpoBdy = document.querySelector("body");
 const corpo = document.querySelector("body").getAttribute("fundo");
-console.log("savaje = " + corpo);
+console.log("imagem de fundo personalizada = " + corpo);
 
-if (!corpo.includes("nada")){
-    console.log("img personalizado");
-    corpoBdy.style.backgroundImage = `url(${corpo}`;
-    corpoBdy.style.backgroundSize = "cover"; 
+if (!corpo.includes("nada")) {
+  console.log("img personalizado");
+  corpoBdy.style.backgroundImage = `url(${corpo}`;
+  corpoBdy.style.backgroundSize = "cover";
 }
 
-var tema = sessionStorage.getItem("tema");
-if (tema == null){
-    sessionStorage.setItem("tema", false);
-    tema = false
+
+
+// tema claro = false; tema escuro = true
+let tema = sessionStorage.getItem("tema");
+
+if (tema === null) {
+  tema = false;
+  console.log(
+    "não foi possivel regastar tema então foi atribuido a ele: " + tema
+  );
+} else {
+  console.log("tema regastado e seu valor é: " + tema);
 }
 
-function atualizarTema(valor){
-    console.log("bolsonaro")
-    let css = document.getElementById("idCss")
-    if (valor){
-        css.setAttribute("href", "temaClaro.css");
-    } else{
-        css.setAttribute("href", "temaEscuro.css");
+atualizarTema();
+
+// botão de mudança de tema
+function mudarTema() {
+  console.log("alterando tema de: " + tema);
+  tema = !tema;
+  console.log("alterando tema para: " + tema);
+  sessionStorage.setItem("tema", tema);
+  atualizarTema();
+}
+
+function atualizarTema() {
+  const css = document.getElementById("idCss");
+  const icone = document.getElementById("bolsonaro_de_calcinha");
+  
+  if (tema) {
+    css.setAttribute("href", "./temaEscuro.css");
+    icone.style.filter = "var(--icone-escuro)";
+    icone.setAttribute("src", "imagens/moon-svgrepo-com.svg");
+    icone.style.transform = "translateX(140%)";
+
+    if (!corpo.includes("nada")) {
+      corpoBdy.style.backdropFilter = "blur(10px) brightness(45%)";
     }
-}
+  } else {
+    css.setAttribute("href", "./temaClaro.css");
+    icone.style.filter = "var(--icone-claro)";
+    icone.setAttribute("src", "imagens/sun-svgrepo-com.svg");
+    icone.style.transform = "";
 
-function mudarTema(){
-    var icone = document.getElementById("bolsonaro_de_calcinha");
-    console.log("1")
-    if (tema == true){
-        tema = false;
-        icone.style.transform = "translateX(110%)";
-        icone.style.filter = "invert(95%) sepia(9%) saturate(829%) hue-rotate(124deg) brightness(91%) contrast(94%) "
-        icone.src = "imagens/moon-svgrepo-com.svg"
-        if (corpo != "nada") {
-            corpoBdy.style.backdropFilter = "blur(10px) brightness(50%)";
-        }
-        console.log("2")
-        atualizarTema(tema);
-    } else {
-        tema = true;
-        icone.style.transform = "translateX(0%)";
-        icone.style.filter = "invert(77%) sepia(58%) saturate(392%) hue-rotate(12deg) brightness(107%) contrast(105%)";
-        icone.src = "imagens/sun-svgrepo-com.svg"
-        if (corpo != "nada") {
-            corpoBdy.style.backdropFilter = "blur(10px) brightness(100%)";
-        }
-        console.log("3")
-        atualizarTema(tema);
+    if (!corpo.includes("nada")) {
+      corpoBdy.style.backdropFilter = "blur(10px) brightness(100%)";
     }
-    console.log("4")
-    sessionStorage.setItem("tema",tema);
-    console.log(tema);
+  }
 }
